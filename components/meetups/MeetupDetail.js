76b11b5fa119f47307d1cebaa9820d779b3ecb0e
Giuseppe Perna <giuseppe@bluelabs.it>
2021-10-04 16:32:22 +0200

    MeetupDetails dynamic page added

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,16 @@
+import classes from './MeetupDetail.module.scss';
+function MeetupDetail(props) {
+    return (
+        <section className={classes.details}>
+            <img
+                src={props.image}
+                alt={props.title}
+            />
+            <h1>{props.title}</h1>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+        </section>
+    );
+}
+
+export default MeetupDetail;
